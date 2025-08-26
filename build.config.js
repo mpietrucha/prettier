55a92c3d353a058e053151a2a81d9cf0f17601e0
Michał Pietrucha <mpietrucha50@gmail.com>
2025-08-26 11:25:35 +0200

    wip

--- build.config.js
+++ build.config.js
@@ -1,8 +1,16 @@
-import { resolve } from 'path'
+import { stat, writeFile } from 'fs/promises'
+import { join, resolve } from 'path'
 import { defineBuildConfig } from 'unbuild'
+import { stringify } from 'yaml'
+
+export const declaration = false
 
 export const alias = {
     '@': resolve('src'),
+}
+
+const yaml = {
+    name: '.prettierrc',
 }
 
 export const rollup = {
@@ -10,6 +18,26 @@ export const rollup = {
     output: { exports: 'default' },
 }
 
-export const declaration = false
+const hooks = {
+    'copy:done': async context => {
+        const { name: path, plugins, extension = 'js' } = context.options.yaml
 
-export default defineBuildConfig({ alias, rollup, declaration })
+        const { input } = context.options.entries.pop()
+
+        const { default: content } = await import(`${input}.${extension}`)
+
+        const output = join(context.options.outDir, path)
+
+        if (!plugins) {
+            content.plugins = undefined
+        }
+
+        await writeFile(output, stringify(content))
+
+        const { size: bytes } = await stat(output)
+
+        context.buildEntries.push({ path, bytes })
+    },
+}
+
+export default defineBuildConfig({ alias, rollup, yaml, declaration, hooks })
